7f916a4b84bfb1a63c1b0e9e29f95b950ccba8cb
Azhara0205 <azhar.tursunova2001@gmail.com>
2024-10-29 15:38:55 +0100

    Create conftest.py

---
+++ DilpomPythonProject/tests/conftest.py
@@ -0,0 +1,17 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+
+@pytest.fixture(scope="session")
+def driver():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # Запуск в безголовом режиме
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    yield driver
+    driver.quit()
